6b7b5f954bda26906508320453ccb31eda1a3b67
ezras256 <120516359+ezras256@users.noreply.github.com>
2024-08-31 05:21:40 +0000

    transitions

--- base-templates/static/js/base.js
+++ base-templates/static/js/base.js
@@ -1,13 +1,28 @@
 function setPreviousPage(siteName) { 
-    // alert(getCookie("currentPage")); 
-    if (siteName == "home") {
-      document.getElementById("old-title-extension-text").textContent = "";
-    } else {
-      document.getElementById("old-title-extension-text").textContent = " - " + getCookie("currentPage").toUpperCase();
-    }
+    document.cookie = "currentPage=" + siteName + ";";
+}
 
-    document.cookie = "currentPage=" + siteName + "; path=/";
+function setText() {
+  let text = document.getElementById("old-title-extension-text");
+  
+  text.textContent = " - " + getCookie("currentPage").toUpperCase();
+  updateTitle(); 
+  console.log(getCookie("currentPage")); 
 
+}
+
+function updateTitle() {
+  let titleStyle = document.getElementById("title").style;
+  if (getCookie("currentPage") == "about") {
+    titleStyle.left = "28.5vw";
+  } else if (getCookie("currentPage") == "projects") {
+    titleStyle.left = "26.5vw";
+  } else if (getCookie("currentPage") == "contact") {
+    titleStyle.left = "27vw";
+  } else {
+    titleStyle.left = "35vw";
+  } 
+  
 }
 
 function getCookie(cname) {
